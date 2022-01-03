ace970b7ec05333cf08fd1034b356fb04f032299
batch-05 <nasir.joypurhat@gmail.com>
2022-01-03 20:11:43 +0600

    first commit java

---
+++ javascript.js
@@ -0,0 +1,20 @@
+document.write("Name : Nasir Uddin </br>");
+document.write("Phone : 01714771225 </br>");
+//comment 
+//document.write("Bangladesh");
+console.log("Output console");
+//variable
+var name = "Nasir Uddin";
+let roll = 44;
+const calss = 'first name"Nasir Uddin" last name';
+document.write(name + "<br>");
+document.write(roll + "<br>");
+document.write(calss);
+
+//variable type
+var num = 55;
+var num1 = "jfhuiafg"
+var num2=2.5635;
+console.log(typeof (num));
+console.log(typeof (num1));
+console.log(num2.toFixed(2));
